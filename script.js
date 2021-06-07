f97320ca463f16d25c33a01777b457b682c7b060
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-06-07 11:54:25 -0300

    usando as acoes do usuario, e dependendo da direcao tem um valor exato para essa ação.

--- script.js
+++ script.js
@@ -21,29 +21,43 @@ function criarCobrinha() {
   }
 }
 
+// escutando o presionamento das teclas que o usuario insere
+document.addEventListener("keydown", update);
+
+function update(event) {
+  //  usando as acoes do usuario, e dependendo da direcao tem um valor exato para essa ação.
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo() {
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
   criarBG();
   criarCobrinha();
+  // inicializando as cordenadas
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
   //cordenadas
-  if (direction == 'right') snakeX += box;
-  if (direction == 'left') snakeX -= box;
-  if (direction == 'up') snakeY -= box;
-  if (direction == 'down') snakeY += box;
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
 
   snake.pop();
 
   let newHead = {
     x: snakeX,
-    y: snakeY
-  }
+    y: snakeY,
+  };
 
   snake.unshift(newHead);
-  
-
 }
 
 let jogo = setInterval(iniciarJogo, 100);
